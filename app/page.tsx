23be7d4eafed906d277036f940340de16bbe4d51
trace2798 <113078518+trace2798@users.noreply.github.com>
2025-06-23 05:48:22 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { ImageUploader } from "@/components/image-uploader";
+import { db } from "@/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const users = await db.query.user.findMany()
+  console.log(users);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     {users.length}
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ImageUploader collectionId="" userId="" workspaceId="" />
         <Image
